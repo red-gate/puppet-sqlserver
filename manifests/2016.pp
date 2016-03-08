d52aba20ce251910f4330945277f90c44ff5470e
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-03-08 10:29:47 +0000

    Make a more generic manifest to install multiple versions of SQL Server 2016 CTPs/RCs

---
+++ manifests/2016.pp
@@ -0,0 +1,63 @@
+# Install SQL Server 2016
+#
+# $source: path to to the SQL Server 2016 Iso file.
+#     (can be a UNC share / URL)
+#
+# $programEntryName: the entry in Add/Remove Programs that is created when installing SQL Server.
+#      This might change based on the version of the iso.
+#
+# $tempFolder: path to a local folder where the SQL Server iso will be downloaded/extracted.
+#
+# $instanceName: The name of the SQL Server instance.
+#
+# $version: the version that will be set as the value of the SQLSERVER_VERSION environment variable.
+#   (used to keep track of what version is installed)
+#
+# $saPassword: The password for the sa account.
+class sqlserver::2016(
+  $source,
+  $programEntryName = 'Microsoft SQL Server 2016 RC0 (64-bit)',
+  $tempFolder = 'c:/temp',
+  $instanceName = 'SQL2016',
+  $version     = '2016RC0',
+  $saPassword) {
+
+  include archive
+
+  $isofilename = inline_template('<%= File.basename(@source) %>')
+  $isofilename_notextension = inline_template('<%= File.basename(@source) %>', '.*')
+
+  ensure_resource('file', $tempFolder, { ensure => directory })
+
+  file { "${tempFolder}/${isofilename_notextension}":
+    ensure  => directory,
+    require => File[$tempFolder],
+  }
+  ->
+  archive { "${tempFolder}/${isofilename}":
+    source       => $source,
+    extract      => true,
+    extract_path => "${tempFolder}/${isofilename_notextension}",
+    creates      => "${tempFolder}/${isofilename_notextension}/setup.exe",
+    cleanup      => true,
+  }
+  ->
+  package { $programEntryName:
+    ensure          => installed,
+    source          => "${tempFolder}/${isofilename_notextension}/setup.exe",
+    install_options => [
+      '/Q',
+      '/IACCEPTSQLSERVERLICENSETERMS',
+      '/ACTION=install',
+      '/FEATURES=SQL,IS,Tools',
+      "/INSTANCENAME=${instanceName}",
+      '/SQLSYSADMINACCOUNTS=BUILTIN\Administrators',
+      '/SQLSVCACCOUNT=NT AUTHORITY\SYSTEM',
+      '/SECURITYMODE=SQL',
+      "/SAPWD=\"${saPassword}\"",
+      '/FILESTREAMLEVEL=2',
+      "/FILESTREAMSHARENAME=${instanceName}"],
+  }
+  ->
+  windows_env { "SQLSERVER_VERSION=${version}": }
+}
