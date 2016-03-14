d97f04d20fa321e123d178b7633f9302b5e887cc
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-03-14 09:32:30 +0000

    Allow setting up max memory for SQL Server 2000

--- manifests/options/base.pp
+++ manifests/options/base.pp
@@ -1,5 +1,17 @@
 # Setup a SQL Server options using sqlcmd.exe and sp_configure
-define sqlserver::options::base($server, $option_name, $option_value, $username = undef, $password = undef) {
+#
+# $issql2000: table/column/options names can be different if targeting SQL Server 2000. Set this to $true if targeting SQL Server 2000
+define sqlserver::options::base($server, $option_name, $option_value, $username = undef, $password = undef, $issql2000 = false) {
+
+  if $issql2000 {
+    $configuration_table = 'sysconfigures'
+    $configuration_name_column = 'comment'
+    $configuration_value_column = 'value'
+  } else {
+    $configuration_table = 'sys.configurations'
+    $configuration_name_column = 'name'
+    $configuration_value_column = 'value_in_use'
+  }
 
   ensure_resource('sqlserver::sqlcmd',
     "${server} - Set show advanced options to 1",
@@ -8,7 +20,7 @@ define sqlserver::options::base($server, $option_name, $option_value, $username 
       'username' => $username,
       'password' => $password,
       'query'    => "exec sp_configure 'show advanced options', 1; reconfigure WITH override",
-      'unless'   => "IF NOT EXISTS(SELECT * FROM sys.configurations WHERE name = 'show advanced options' and value_in_use = 1) raiserror ('Wrong value is in use',1,1)",
+      'unless'   => "IF NOT EXISTS(SELECT * FROM ${configuration_table} WHERE ${configuration_name_column} = 'show advanced options' and ${configuration_value_column} = 1) raiserror ('Wrong value is in use',1,1)",
     })
 
   sqlserver::sqlcmd { "${server} - Set ${option_name} to ${option_value}":
@@ -16,7 +28,7 @@ define sqlserver::options::base($server, $option_name, $option_value, $username 
     username => $username,
     password => $password,
     query    => "exec sp_configure '${option_name}', ${option_value}; reconfigure WITH override",
-    unless   => "IF NOT EXISTS(SELECT * FROM sys.configurations WHERE name = '${option_name}' and value_in_use = ${option_value}) raiserror ('Wrong value is in use',1,1)",
+    unless   => "IF NOT EXISTS(SELECT * FROM ${configuration_table} WHERE ${configuration_name_column} = '${option_name}' and ${configuration_value_column} = ${option_value}) raiserror ('Wrong value is in use',1,1)",
     require  => Sqlserver::Sqlcmd["${server} - Set show advanced options to 1"],
   }
 
--- manifests/options/max_memory.pp
+++ manifests/options/max_memory.pp
@@ -1,11 +1,20 @@
 # Setup the 'max server memory (MB)' option
-define sqlserver::options::max_memory($server, $value, $username = undef, $password = undef) {
+#
+# $issql2000: table/column/options names can be different if targeting SQL Server 2000. Set this to $true if targeting SQL Server 2000
+define sqlserver::options::max_memory($server, $value, $username = undef, $password = undef, $issql2000 = false) {
+
+  if $issql2000 {
+    $option_name = 'Maximum size of server memory (MB)'
+  } else {
+    $option_name = 'max server memory (MB)'
+  }
 
   sqlserver::options::base { "${server} - Setting Max Memory to ${value}MB":
     server       => $server,
     username     => $username,
     password     => $password,
-    option_name  => 'max server memory (MB)',
+    option_name  => $option_name,
     option_value => $value,
+    issql2000    => $issql2000,
   }
 }
