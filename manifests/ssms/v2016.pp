17b0422ee4ae75b849369f5f2b4ce9ecc526d0da
nico.vanelslande <nico.vanelslande@red-gate.com>
2016-05-12 15:28:26 +0100

    quick manifest to install SSMS 2016 Preview.

---
+++ manifests/ssms/v2016.pp
@@ -0,0 +1,23 @@
+# Install SSMS 2016.
+class sqlserver::ssms::v2016(
+  $source = 'http://go.microsoft.com/fwlink/?LinkID=786460',
+  $filename = 'SSMS-Setup-ENU.exe',
+  $programName = 'Microsoft SQL Server Management Studio - April 2016',
+  $tempFolder = 'c:/temp'
+  ) {
+
+  require archive
+
+  ensure_resource('file', $tempFolder, { ensure => directory })
+
+  archive { "${tempFolder}/${filename}":
+    source  => $source,
+    require => File[$tempFolder],
+  }
+  ->
+  package { $programName:
+    ensure          => installed,
+    source          => "${tempFolder}/${filename}",
+    install_options => ['/install', '/quiet', '/norestart']
+  }
+}
