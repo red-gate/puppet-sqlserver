009e11feb97752420518067301510864158c9d48
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-02 09:38:53 +0000

    Use full path to sqllocaldb in sqlserver::localdb::instance

--- manifests/localdb/instance.pp
+++ manifests/localdb/instance.pp
@@ -4,6 +4,8 @@
 # localdb_instance_name: the name of the localdb instance to create
 define sqlserver::localdb::instance($localdb_sql_version = '110', $localdb_instance_name = 'v11.0') {
 
+  $sqllocaldb = "& \"C:/Program Files/Microsoft SQL Server/${localdb_sql_version}/Tools/Binn/sqllocaldb.exe\""
+
   Exec {
     cwd      => "C:/Program Files/Microsoft SQL Server/${localdb_sql_version}/Tools/Binn",
     provider => 'powershell',
@@ -11,14 +13,16 @@ define sqlserver::localdb::instance($localdb_sql_version = '110', $localdb_insta
 
   # This command will delete $localdb_instance_name if it is not valid.
   exec { "sqlserver::localdb::instance-delete-${localdb_instance_name}-if-invalid":
-    command => "sqllocaldb delete ${localdb_instance_name}",
-    onlyif  => "if((sqllocaldb info ${localdb_instance_name}) -like 'Name:*${localdb_instance_name}') { exit 1 }",
+    command   => "${sqllocaldb} delete ${localdb_instance_name}",
+    onlyif    => "if((${sqllocaldb} info ${localdb_instance_name}) -like 'Name:*${localdb_instance_name}') { exit 1 }",
+    logoutput => true,
   }
   ->
   # This command will create $localdb_instance_name if it does not exist
   exec { "sqlserver::localdb::instance-create-${localdb_instance_name}-if-missing":
-    command => "sqllocaldb create ${localdb_instance_name}",
-    unless  => "if((sqllocaldb info ${localdb_instance_name}) -like '*\"${localdb_instance_name}\" is not created.') { exit 1 }",
+    command   => "${sqllocaldb} create ${localdb_instance_name}",
+    unless    => "if((${sqllocaldb} info ${localdb_instance_name}) -like '*\"${localdb_instance_name}\" is not created.') { exit 1 }",
+    logoutput => true,
   }
 
 }
