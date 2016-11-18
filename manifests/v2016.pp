eb724a4b5170418ce56d6e1089bf3ea0b280d350
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-18 15:44:54 +0000

    [breaking change] puppet-lint for puppet v4

--- manifests/v2016.pp
+++ manifests/v2016.pp
@@ -3,25 +3,26 @@
 # $source: path to to the SQL Server 2016 Iso file.
 #     (can be a UNC share / URL)
 #
-# $programEntryName: the entry in Add/Remove Programs that is created when installing SQL Server.
+# $sa_password: The password for the sa account.
+#
+# $program_entry_name: the entry in Add/Remove Programs that is created when installing SQL Server.
 #      This might change based on the version of the iso.
 #
-# $tempFolder: path to a local folder where the SQL Server iso will be downloaded/extracted.
+# $temp_folder: path to a local folder where the SQL Server iso will be downloaded/extracted.
 #
-# $instanceName: The name of the SQL Server instance.
+# $instance_name: The name of the SQL Server instance.
 #
 # $version: the version that will be set as the value of the SQLSERVER_VERSION environment variable.
 #   (used to keep track of what version is installed)
 #
-# $saPassword: The password for the sa account.
 class sqlserver::v2016(
   $source,
-  $programEntryName = 'Microsoft SQL Server 2016 RC0 (64-bit)',
-  $tempFolder       = 'c:/temp',
-  $instanceName     = 'SQL2016',
-  $version          = '2016RC0',
-  $reboot_timeout   = 60,
-  $saPassword) {
+  $sa_password,
+  $program_entry_name = 'Microsoft SQL Server 2016 (64-bit)',
+  $temp_folder        = 'c:/temp',
+  $instance_name      = 'SQL2016',
+  $version            = '2016RTM',
+  $reboot_timeout     = 60) {
 
   require chocolatey
   include archive
@@ -30,36 +31,36 @@ class sqlserver::v2016(
   $isofilename = inline_template('<%= File.basename(@source) %>')
   $isofilename_notextension = inline_template('<%= File.basename(@source, ".*") %>')
 
-  ensure_resource('file', $tempFolder, { ensure => directory })
+  ensure_resource('file', $temp_folder, { ensure => directory })
 
-  file { "${tempFolder}/${isofilename_notextension}":
+  file { "${temp_folder}/${isofilename_notextension}":
     ensure  => directory,
-    require => File[$tempFolder],
+    require => File[$temp_folder],
   }
   ->
-  archive { "${tempFolder}/${isofilename}":
+  archive { "${temp_folder}/${isofilename}":
     source       => $source,
     extract      => true,
-    extract_path => "${tempFolder}/${isofilename_notextension}",
-    creates      => "${tempFolder}/${isofilename_notextension}/setup.exe",
+    extract_path => "${temp_folder}/${isofilename_notextension}",
+    creates      => "${temp_folder}/${isofilename_notextension}/setup.exe",
     cleanup      => true,
   }
   ->
-  package { $programEntryName:
+  package { $program_entry_name:
     ensure          => installed,
-    source          => "${tempFolder}/${isofilename_notextension}/setup.exe",
+    source          => "${temp_folder}/${isofilename_notextension}/setup.exe",
     install_options => [
       '/Q',
       '/IACCEPTSQLSERVERLICENSETERMS',
       '/ACTION=install',
       '/FEATURES=SQL,IS,Tools',
-      "/INSTANCENAME=${instanceName}",
+      "/instance_name=${instance_name}",
       '/SQLSYSADMINACCOUNTS=BUILTIN\Administrators',
       '/SQLSVCACCOUNT=NT AUTHORITY\SYSTEM',
       '/SECURITYMODE=SQL',
-      "/SAPWD=\"${saPassword}\"",
+      "/SAPWD=\"${sa_password}\"",
       '/FILESTREAMLEVEL=2',
-      "/FILESTREAMSHARENAME=${instanceName}"],
+      "/FILESTREAMSHARENAME=${instance_name}"],
     require         => Reboot['reboot before installing SQL Server (if pending)'],
   }
   ~>
