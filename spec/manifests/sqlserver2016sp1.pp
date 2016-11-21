6909362e43e651df57a2e501fd1d93a38916e405
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-21 11:30:48 +0000

    stick with the defaults when testing.

--- spec/manifests/sqlserver2016sp1.pp
+++ spec/manifests/sqlserver2016sp1.pp
@@ -1,6 +1,5 @@
 class { '::sqlserver::v2016':
-  source             => $::sqlserver2016_iso_url,
-  sa_password        => 'sdf347RT!',
-  program_entry_name => 'Microsoft SQL Server 2016 (64-bit)',
-  install_type       => 'SP1',
+  source       => $::sqlserver2016_iso_url,
+  sa_password  => 'sdf347RT!',
+  install_type => 'SP1',
 }
