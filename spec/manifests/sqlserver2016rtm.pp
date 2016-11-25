658bc5b74512bfb9c061a01a40412c1f5a0151f0
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-25 15:19:37 +0000

    Update kitchen test harness to setup and test 2 instances of SQL Server 2016 SP1

--- spec/manifests/sqlserver2016rtm.pp
+++
@@ -1,5 +0,0 @@
-class { '::sqlserver::v2016':
-  source       => $::sqlserver2016_iso_url,
-  sa_password  => 'sdf347RT!',
-  install_type => 'RTM',
-}
---
+++ spec/manifests/sqlserver2016sp1_2_instances.pp
@@ -0,0 +1,16 @@
+class { '::sqlserver::v2016::resources':
+  source       => $::sqlserver2016_iso_url,
+  install_type => 'SP1',
+}
+
+sqlserver::v2016::instance { 'SQL2016_1':
+  sa_password  => 'sdf347RT!',
+  install_type => 'SP1',
+  require      => Class['::sqlserver::v2016::resources'],
+}
+
+sqlserver::v2016::instance { 'SQL2016_2':
+  sa_password  => 'sdf347RT!',
+  install_type => 'SP1',
+  require      => Class['::sqlserver::v2016::resources'],
+}
