c1b3c0e69dbf43c13afa18ca0fa4e2db6b609fce
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-30 14:40:14 +0000

    Validate expected services are running

--- spec/acceptance/sqlserver2016rtm_spec.rb
+++ spec/acceptance/sqlserver2016rtm_spec.rb
@@ -9,3 +9,12 @@ end
 describe package('SQL Server 2016 Database Engine Services') do
   it { should be_installed.with_version('13.0.1601.5') }
 end
+
+['SQLBrowser', 'MSSQL$SQL2016', 'SQLAGENT$SQL2016'].each do |service_name|
+  describe service(service_name) do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+    it { should have_start_mode('Automatic') }
+  end
+end
