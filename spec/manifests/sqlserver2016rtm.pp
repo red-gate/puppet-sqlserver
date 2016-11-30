fcd88f0f44f1cca50ce276b2bde134df202ca926
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-30 14:09:30 +0000

    Test setting options

--- spec/manifests/sqlserver2016rtm.pp
+++ spec/manifests/sqlserver2016rtm.pp
@@ -7,3 +7,16 @@ class { '::sqlserver::v2016':
   sa_password  => 'sdf347RT!',
   install_type => 'RTM',
 }
+
+# Test setting options
+
+sqlserver::options::clr_enabled { 'SQL2016: clr enabled':
+  server  => 'localhost\SQL2016',
+  require => Class['::sqlserver::v2016'],
+  value   => 1,
+}
+sqlserver::options::max_memory { 'SQL2016: Max Memory':
+  server  => 'localhost\SQL2016',
+  require => Class['::sqlserver::v2016'],
+  value   => 512,
+}
