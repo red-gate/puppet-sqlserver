4e3b7822dc8ddaa43fe39dd2a832543938106bb2
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-12-05 11:58:27 +0000

    Allow setting up Windows logins and roles.

---
+++ manifests/users/login_role.pp
@@ -0,0 +1,12 @@
+# Assign a role to a login
+define sqlserver::users::login_role($server, $login_name, $role_name, $query_username = undef, $query_password = undef) {
+
+  ::sqlserver::sqlcmd::sqlquery { "${server} - Add role ${role_name} to ${login_name} login":
+    server   => $server,
+    username => $query_username,
+    password => $query_password,
+    query    => "EXEC sp_addsrvrolemember '${login_name}', '${role_name}'",
+    unless   => "IF (SELECT IS_SRVROLEMEMBER ('${role_name}', '${login_name}')) != 1 raiserror ('Role is not assigned yet',1,1)",
+  }
+
+}
--- spec/manifests/sqlserver2016rtm.pp
+++ spec/manifests/sqlserver2016rtm.pp
@@ -20,3 +20,17 @@ sqlserver::options::max_memory { 'SQL2016: Max Memory':
   require => Class['::sqlserver::v2016'],
   value   => 512,
 }
+
+# Test logins/roles
+sqlserver::users::login_windows { 'SQL2016: Everyone login':
+  server     => 'localhost\SQL2016',
+  login_name => '\Everyone',
+  require    => Class['::sqlserver::v2016'],
+}
+->
+sqlserver::users::login_role { 'SQL2016: Everyone is sysadmin':
+  server     => 'localhost\SQL2016',
+  login_name => '\Everyone',
+  role_name  => 'sysadmin',
+  require    => Class['::sqlserver::v2016'],
+}
