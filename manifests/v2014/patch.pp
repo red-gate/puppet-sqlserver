f96be5ea36a9c7e5a9a2c50bb42ddd4fe9e8fae4
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-12-21 14:11:06 +0000

    Fix SQL Server 2014 SP2 Patch level value.

--- manifests/v2014/patch.pp
+++ manifests/v2014/patch.pp
@@ -2,7 +2,7 @@
 # SQL Server 2014 build versions: https://support.microsoft.com/en-gb/kb/2936603
 class sqlserver::v2014::patch(
   $source = 'https://download.microsoft.com/download/6/D/9/6D90C751-6FA3-4A78-A78E-D11E1C254700/SQLServer2014SP2-KB3171021-x64-ENU.exe',
-  $version = '12.0.5000.0'
+  $version = '12.2.5000.0'
   ) {
   # $installer points to setup.exe
   $installer = inline_template('<%= "C:/Windows/Temp/" + File.basename(@source, ".*") + "/setup.exe" %>')
