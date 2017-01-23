61ceee6e3278810acf63dfced449c4c793399375
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-01-23 09:21:05 +0000

    fix some download urls that do not seem to be valid anymore

--- manifests/sqlcmd/install.pp
+++ manifests/sqlcmd/install.pp
@@ -7,10 +7,10 @@ class sqlserver::sqlcmd::install($tempFolder = 'C:/temp') {
   ensure_resource('file', $tempFolder, { ensure => directory })
 
   if $::architecture == 'x86' {
-    $odbcdriver_source = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/1033/x86/msodbcsql.msi'
+    $odbcdriver_source = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/ENU/x86/msodbcsql.msi'
     $sqlcmdutils_source = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x86/MsSqlCmdLnUtils.msi'
   } else {
-    $odbcdriver_source = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/1033/amd64/msodbcsql.msi'
+    $odbcdriver_source = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/ENU/x64/msodbcsql.msi'
     $sqlcmdutils_source = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x64/MsSqlCmdLnUtils.msi'
   }
 
