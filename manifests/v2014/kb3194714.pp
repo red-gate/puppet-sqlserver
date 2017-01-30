39a73c3f3f566f295a947b69b72bee87a914e00a
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-01-30 10:35:14 +0000

    keep track of the build numbers in the comments.

--- manifests/v2014/kb3194714.pp
+++ manifests/v2014/kb3194714.pp
@@ -1,4 +1,4 @@
-# Install "Security Update for SQL Server 2014 Service Pack 2 GDR (KB3194714)"
+# Install "Security Update for SQL Server 2014 Service Pack 2 GDR (KB3194714)" (12.2.5203.0)
 # (https://www.microsoft.com/en-us/download/details.aspx?id=54190)
 class sqlserver::v2014::kb3194714(
   $source = 'https://download.microsoft.com/download/A/4/4/A44A125B-5900-4877-891B-4FE497600419/SQL2014SP2GDR/x64/SQLServer2014-KB3194714-x64.exe',
