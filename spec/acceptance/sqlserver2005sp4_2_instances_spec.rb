113752aa77a8b01a09b5d604248542356e6bcaf2
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-04-03 16:48:57 +0100

    test harness for SQL Server 2005 setup

---
+++ spec/acceptance/sqlserver2005sp4_2_instances_spec.rb
@@ -0,0 +1,36 @@
+# Encoding: utf-8
+require_relative 'spec_windowshelper'
+
+describe package('Microsoft SQL Server 2005 (64-bit)') do
+  it { should be_installed }
+end
+
+['SQL2005_1', 'SQL2005_2'].each do |instance_name|
+  describe service("MSSQL$#{instance_name}") do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+    it { should have_start_mode('Automatic') }
+  end
+
+  describe windows_registry_key("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SQL Server\\MSSQL9.#{instance_name}\\Setup") do
+    it { should exist }
+    it { should have_property_value('PatchLevel', :type_string, '10.4.6000.29') }
+  end
+end
+
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL9.SQL2005_1\Setup') do
+  it { should have_property_value('Collation', :type_string, 'Latin1_General_CI_AS') }
+end
+
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL9.SQL2005_1\Mssqlserver\Supersocketnetlib\tcp\ipall') do
+  it { should have_property_value('tcpport', :type_string, '1433') }
+end
+
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL9.SQL2005_2\Setup') do
+  it { should have_property_value('Collation', :type_string, 'Latin1_General_CS_AS_KS_WS') }
+end
+
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL9.SQL2005_2\Mssqlserver\Supersocketnetlib\tcp\ipall') do
+  it { should have_property_value('tcpport', :type_string, '1434') }
+end
