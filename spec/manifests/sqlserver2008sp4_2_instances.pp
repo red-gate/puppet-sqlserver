69ba63054afcd1c91104b23e4b222dc16314701e
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-04-25 15:39:39 +0100

    Set some SQL Server options during the tests

--- spec/manifests/sqlserver2008sp4_2_instances.pp
+++ spec/manifests/sqlserver2008sp4_2_instances.pp
@@ -22,3 +22,35 @@ sqlserver::v2008::instance { 'SQL2008_2':
   },
   tcp_port       => 1434,
 }
+
+# Test setting options with the first instance
+
+sqlserver::options::clr_enabled { 'SQL2008_1: clr enabled':
+  server  => 'localhost\SQL2008_1',
+  require => Sqlserver::V2008::Instance['localhost\SQL2008_1'],
+  value   => 1,
+}
+sqlserver::options::max_memory { 'SQL2008_1: Max Memory':
+  server  => 'localhost\SQL2008_1',
+  require => Sqlserver::V2008::Instance['localhost\SQL2008_1'],
+  value   => 512,
+}
+sqlserver::options::xp_cmdshell { 'SQL2008_1: xp_cmdshell':
+  server  => 'localhost\SQL2008_1',
+  require => Sqlserver::V2008::Instance['localhost\SQL2008_1'],
+  value   => 1,
+}
+
+# Test logins/roles
+sqlserver::users::login_windows { 'SQL2008_1: Everyone login':
+  server     => 'localhost\SQL2008_1',
+  login_name => '\Everyone',
+  require    => Sqlserver::V2008::Instance['localhost\SQL2008_1'],
+}
+->
+sqlserver::users::login_role { 'SQL2008_1: Everyone is sysadmin':
+  server     => 'localhost\SQL2008_1',
+  login_name => '\Everyone',
+  role_name  => 'sysadmin',
+  require    => Sqlserver::V2008::Instance['localhost\SQL2008_1'],
+}
--- spec/manifests/sqlserver2017ctp_2_instances.pp
+++ spec/manifests/sqlserver2017ctp_2_instances.pp
@@ -22,3 +22,36 @@ sqlserver::v2017::instance { 'SQL2017_2':
   },
   tcp_port       => 1434,
 }
+
+
+# Test setting options with the first instance
+
+sqlserver::options::clr_enabled { 'SQL2017_1: clr enabled':
+  server  => 'localhost\SQL2017_1',
+  require => Sqlserver::V2017::Instance['localhost\SQL2017_1'],
+  value   => 1,
+}
+sqlserver::options::max_memory { 'SQL2017_1: Max Memory':
+  server  => 'localhost\SQL2017_1',
+  require => Sqlserver::V2017::Instance['localhost\SQL2017_1'],
+  value   => 512,
+}
+sqlserver::options::xp_cmdshell { 'SQL2017_1: xp_cmdshell':
+  server  => 'localhost\SQL2017_1',
+  require => Sqlserver::V2017::Instance['localhost\SQL2017_1'],
+  value   => 1,
+}
+
+# Test logins/roles
+sqlserver::users::login_windows { 'SQL2017_1: Everyone login':
+  server     => 'localhost\SQL2017_1',
+  login_name => '\Everyone',
+  require    => Sqlserver::V2017::Instance['localhost\SQL2017_1'],
+}
+->
+sqlserver::users::login_role { 'SQL2017_1: Everyone is sysadmin':
+  server     => 'localhost\SQL2017_1',
+  login_name => '\Everyone',
+  role_name  => 'sysadmin',
+  require    => Sqlserver::V2017::Instance['localhost\SQL2017_1'],
+}
