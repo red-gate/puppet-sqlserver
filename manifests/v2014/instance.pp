6094ea0eb8d3d35dd20613b97c23cc37cf64be93
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-04-25 11:48:07 +0100

    sqlserver::v2014::instance:install_type can be 'RTM', 'SP1' or 'SP2'

--- manifests/v2014/instance.pp
+++ manifests/v2014/instance.pp
@@ -1,13 +1,10 @@
 # Install an configure a single SQL Server 2014 Instance.
 #
-# $install_type:
-#   'RTM' (don't patch)
-#   or
-#   'Patch' (install the latest Service Pack/Patch/CU we are aware of.)
+# $install_type: 'RTM', 'SP1', 'SP2'
 #
 define sqlserver::v2014::instance(
   $instance_name  = $title,
-  $install_type   = 'Patch',
+  $install_type   = 'SP2',
   $install_params = {},
   $tcp_port       = 0
   ) {
@@ -24,7 +21,8 @@ define sqlserver::v2014::instance(
     install_params => $install_params,
   }
 
-  if $install_type == 'Patch' {
+  # 'Patch' is equivalent to 'SP2' for backwards compatibility
+  if $install_type == 'SP2' or $install_type == 'Patch' {
     require ::sqlserver::v2014::sp2
     require ::sqlserver::v2014::kb3194714
 
@@ -38,6 +36,15 @@ define sqlserver::v2014::instance(
       instance_name      => $instance_name,
       installer_path     => $::sqlserver::v2014::kb3194714::installer,
       applies_to_version => $::sqlserver::v2014::kb3194714::applies_to_version,
+    }
+  }
+  elsif $install_type == 'SP1' {
+    require ::sqlserver::v2014::sp1
+
+    sqlserver::common::patch_sqlserver_instance { "${instance_name}:${::sqlserver::v2014::sp1::installer}":
+      instance_name      => $instance_name,
+      installer_path     => $::sqlserver::v2014::sp1::installer,
+      applies_to_version => $::sqlserver::v2014::sp1::applies_to_version,
     }
   }
 
