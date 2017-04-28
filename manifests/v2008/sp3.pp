26c81eb0fc624a5fb61f624a1afab7e402f36da4
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-04-28 08:51:09 +0100

    Add the SQL Server 2008 SP3 download url :doh:

--- manifests/v2008/sp3.pp
+++ manifests/v2008/sp3.pp
@@ -1,7 +1,7 @@
 # Download and extract SQL Server 2008 Sp3. (10.3.5500.0)
 # SQL Server 2008 build versions: https://sqlserverbuilds.blogspot.co.uk/
 class sqlserver::v2008::sp3(
-  $source = '',
+  $source = 'https://download.microsoft.com/download/7/C/8/7C8F6A23-5876-4C55-91AD-5488360F8FD6/SQLServer2008SP3-KB2546951-x64-ENU.exe',
   $applies_to_version = '10.0.1600.22'
   ) {
   # $installer points to setup.exe
