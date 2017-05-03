1df9ec6bcf6d3d455f88f50f7facda442a275d98
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-05-03 15:21:52 +0100

    puppet-lint

--- manifests/sqlcmd/sqlquery.pp
+++ manifests/sqlcmd/sqlquery.pp
@@ -4,20 +4,20 @@ define sqlserver::sqlcmd::sqlquery($server, $query, $unless = undef, $username =
   require sqlserver::sqlcmd::install
 
   if $username {
-    $authArguments = "-U \"${username}\" -P \"${password}\""
+    $auth_arguments = "-U \"${username}\" -P \"${password}\""
   } else {
-    $authArguments = '-E'
+    $auth_arguments = '-E'
   }
 
   if $unless {
-    $unlesssqlcmd = "sqlcmd.exe -b -V 1 -S ${server} ${authArguments} -Q \"${unless}\""
+    $unlesssqlcmd = "sqlcmd.exe -b -V 1 -S ${server} ${auth_arguments} -Q \"${unless}\""
   } else {
     $unlesssqlcmd = undef
   }
 
   exec { "${title} - ${query}":
     path    => $sqlserver::sqlcmd::install::paths,
-    command => "sqlcmd.exe -b -V 1 -S ${server} ${authArguments} -Q \"${query}\"",
+    command => "sqlcmd.exe -b -V 1 -S ${server} ${auth_arguments} -Q \"${query}\"",
     unless  => $unlesssqlcmd,
   }
 
