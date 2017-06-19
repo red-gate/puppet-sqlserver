9ea7ca27f9b399e3a72c7ae02ec5185184958a71
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-06-19 14:22:56 +0100

    test setting options for SQL Server 2000 and 2005

--- spec/manifests/sqlserver2000sp3_2_instances.pp
+++ spec/manifests/sqlserver2000sp3_2_instances.pp
@@ -26,3 +26,29 @@ sqlserver::v2000::instance { 'SQL2000_1':
   sqlcollation          => 'Latin1_General_CS_AS_KS_WS',
   tcp_port              => 1434,
 }
+
+# Test setting options with the first instance
+
+class { 'sqlserver::sqlcmd::install':
+  version => '10', # v10 can connect to SQL Server 2000. v11 refuses to.
+}
+
+sqlserver::options::max_memory { 'SQL2000_1: Max Memory':
+  server  => 'localhost\SQL2000_1',
+  require => Sqlserver::V2000::Instance['SQL2000_1'],
+  value   => 512,
+}
+
+# Test logins/roles
+sqlserver::users::login_windows { 'SQL2000_1: Everyone login':
+  server     => 'localhost\SQL2000_1',
+  login_name => '\Everyone',
+  require    => Sqlserver::V2000::Instance['SQL2000_1'],
+}
+->
+sqlserver::users::login_role { 'SQL2000_1: Everyone is sysadmin':
+  server     => 'localhost\SQL2000_1',
+  login_name => '\Everyone',
+  role_name  => 'sysadmin',
+  require    => Sqlserver::V2000::Instance['SQL2000_1'],
+}
