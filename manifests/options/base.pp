f7262f9f1897eafc1b2929e69da6c671517b7470
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-06-19 16:51:49 +0100

    For SQL Server 2000 max memory is read using "Maximum size of server memory (MB)" but set using "max server memory (MB)"
    :tired:

--- manifests/options/base.pp
+++ manifests/options/base.pp
@@ -1,7 +1,7 @@
 # Setup a SQL Server options using sqlcmd.exe and sp_configure
 #
 # $issql2000: table/column/options names can be different if targeting SQL Server 2000. Set this to $true if targeting SQL Server 2000
-define sqlserver::options::base($server, $option_name, $option_value, $username = undef, $password = undef, $issql2000 = false) {
+define sqlserver::options::base($server, $option_name, $option_value, $username = undef, $password = undef, $issql2000 = false, $option_set_name = undef) {
 
   if $issql2000 {
     $configuration_table = 'sysconfigures'
@@ -11,6 +11,12 @@ define sqlserver::options::base($server, $option_name, $option_value, $username 
     $configuration_table = 'sys.configurations'
     $configuration_name_column = 'name'
     $configuration_value_column = 'value_in_use'
+  }
+
+  # Allow override option_name for SQL Server 2000 which has different values when setting/reading options (!)
+  $option_name_to_set = $option_set_name ? {
+    undef   => $option_name,
+    default => $option_set_name
   }
 
   ensure_resource('sqlserver::sqlcmd::sqlquery',
@@ -27,7 +33,7 @@ define sqlserver::options::base($server, $option_name, $option_value, $username 
     server   => $server,
     username => $username,
     password => $password,
-    query    => "exec sp_configure '${option_name}', ${option_value}; reconfigure WITH override",
+    query    => "exec sp_configure '${option_name_to_set}', ${option_value}; reconfigure WITH override",
     unless   => "IF NOT EXISTS(SELECT * FROM ${configuration_table} WHERE ${configuration_name_column} = '${option_name}' and ${configuration_value_column} = ${option_value}) raiserror ('Wrong value is in use',1,1)",
     require  => Sqlserver::Sqlcmd::Sqlquery["${server} - Set show advanced options to 1"],
   }
