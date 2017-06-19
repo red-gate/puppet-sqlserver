c6065ee04b60b2fa018fa70943916248c373cda3
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-06-19 14:21:10 +0100

    allow installing an older version of sqlcmd that can connect to SQL Server 2000.

---
+++ manifests/sqlcmd/install/v11.pp
@@ -0,0 +1,30 @@
+# Install sqlcmd.exe
+class sqlserver::sqlcmd::install::v11($temp_folder = 'C:/Windows/Temp') {
+
+  include archive
+
+  if $::architecture == 'x86' {
+    $odbcdriver_source = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/ENU/x86/msodbcsql.msi'
+    $sqlcmdutils_source = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x86/MsSqlCmdLnUtils.msi'
+  } else {
+    $odbcdriver_source = 'https://download.microsoft.com/download/5/7/2/57249A3A-19D6-4901-ACCE-80924ABEB267/ENU/x64/msodbcsql.msi'
+    $sqlcmdutils_source = 'https://download.microsoft.com/download/5/5/B/55BEFD44-B899-4B54-ACD7-506E03142B34/1033/x64/MsSqlCmdLnUtils.msi'
+  }
+
+  archive { "${temp_folder}/msodbcsql_v11.msi":
+    source  => $odbcdriver_source,
+  }
+  -> package { 'Microsoft ODBC Driver 11 for SQL Server':
+    ensure          => installed,
+    source          => "${temp_folder}/msodbcsql_v11.msi",
+    install_options => ['ADDLOCAL=ALL', 'IACCEPTMSODBCSQLLICENSETERMS=YES'],
+  }
+  -> archive { "${temp_folder}/MsSqlCmdLnUtils_v11.msi":
+    source  => $sqlcmdutils_source,
+  }
+  -> package { 'Microsoft Command Line Utilities 11 for SQL Server':
+    ensure          => installed,
+    source          => "${temp_folder}/MsSqlCmdLnUtils_v11.msi",
+    install_options => ['IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES'],
+  }
+}
