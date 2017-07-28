4049a718f6cfdbd0ff1fea7ce72c47e34652fec0
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2017-07-28 11:59:41 +0100

    Revert "SQL Server 2000 case sensitive collations specific fix."
    On second though, there is enough SQL Server 2000 related code in this repo. It's 2017.
    Let's move on and not deal with this here...
    
    This reverts commit 20062f41d0678589f48b290934a72d85906a83ff.

--- manifests/users/login_windows.pp
+++ manifests/users/login_windows.pp
@@ -1,27 +1,16 @@
 # Create a Windows authentication login
 define sqlserver::users::login_windows($server, $login_name, $query_username = undef, $query_password = undef, $is_sql_2000 = false) {
 
-  if $is_sql_2000 and $login_name =~ /[\\]/{
-    $split = split($login_name, "\\\\")
-    # For SQL Server 2000, the '<machine-name>' part in the login (<machine-name>\<username>)
-    # must be uppercase. (if not the query will fail on instances configured with a case sensitive collation)
-    $first_part = upcase($split[0])
-    $second_part = $split[1]
-    $fixedup_loginname = "${first_part}\\${second_part}"
-  } else {
-    $fixedup_loginname = $login_name
-  }
-
   $sql_query = $is_sql_2000 ? {
-    true  => "exec sp_grantlogin '${fixedup_loginname}'",
-    false => "CREATE LOGIN [${fixedup_loginname}] FROM WINDOWS",
+    true  => "exec sp_grantlogin '${login_name}'",
+    false => "CREATE LOGIN [${login_name}] FROM WINDOWS",
   }
 
-  ::sqlserver::sqlcmd::sqlquery { "${server} - Create ${fixedup_loginname} login":
+  ::sqlserver::sqlcmd::sqlquery { "${server} - Create ${login_name} login":
     server   => $server,
     username => $query_username,
     password => $query_password,
     query    => $sql_query,
-    unless   => "IF NOT EXISTS(SELECT * from master.dbo.syslogins WHERE loginname = '${fixedup_loginname}') raiserror ('Login does not exist',1,1)",
+    unless   => "IF NOT EXISTS(SELECT * from master.dbo.syslogins WHERE loginname = '${login_name}') raiserror ('Login does not exist',1,1)",
   }
 }
