a7788d39abe5b6e87afb21cdb58a8e50246bf8aa
Nicolas Vanelslande <nicolas.vanelslande@gmail.com>
2018-09-06 09:33:47 +0100

    Apply detected pending reboot immediately
    Because the SQL Server installers will complain anyway.

--- manifests/common/reboot_resources.pp
+++ manifests/common/reboot_resources.pp
@@ -2,10 +2,12 @@
 # SQL Server instances
 define sqlserver::common::reboot_resources($instance_name = $title){
   reboot { "reboot before installing ${instance_name} (if pending)":
-    when => pending,
+    when  => pending,
+    apply => 'immediately',
   }
   reboot { "reboot before installing ${instance_name} Patch (if pending)":
-    when => pending,
+    when  => pending,
+    apply => 'immediately',
   }
   reboot { "Reboot after patching ${instance_name}":
     apply   => finished,
