d0293e2ed65e1fb106b7dcbc45d9f29892a5d636
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2018-10-24 11:01:04 +0100

    Fix specs for 2019

--- spec/acceptance/sqlserver2019rtm_2_instances_spec.rb
+++ spec/acceptance/sqlserver2019rtm_2_instances_spec.rb
@@ -1,7 +1,7 @@
 # Encoding: utf-8
 require_relative 'spec_windowshelper'
 
-describe package('Microsoft SQL Server 2019 (64-bit)') do
+describe package('Microsoft SQL Server * (64-bit)') do
   it { should be_installed }
 end
 
@@ -14,25 +14,25 @@ end
     it { should have_start_mode('Automatic') }
   end
 
-  describe windows_registry_key("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SQL Server\\MSSQL14.#{instance_name}\\Setup") do
+  describe windows_registry_key("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SQL Server\\MSSQL15.#{instance_name}\\Setup") do
     it { should exist }
-    it { should have_property_value('PatchLevel', :type_string, '14.0.1000.169') }
+    it { should have_property_value('PatchLevel', :type_string, '15.0.1000.34') }
   end
 end
 
-describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQL2019_1\Setup') do
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQL2019_1\Setup') do
   it { should have_property_value('Collation', :type_string, 'Latin1_General_CI_AS') }
 end
 
-describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQL2019_1\Mssqlserver\Supersocketnetlib\tcp\ipall') do
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQL2019_1\Mssqlserver\Supersocketnetlib\tcp\ipall') do
   it { should have_property_value('tcpport', :type_string, '1433') }
 end
 
 
-describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQL2019_2\Setup') do
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQL2019_2\Setup') do
   it { should have_property_value('Collation', :type_string, 'Latin1_General_CS_AS_KS_WS') }
 end
 
-describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL14.SQL2019_2\Mssqlserver\Supersocketnetlib\tcp\ipall') do
+describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQL2019_2\Mssqlserver\Supersocketnetlib\tcp\ipall') do
   it { should have_property_value('tcpport', :type_string, '1434') }
 end
