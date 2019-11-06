8ad272ef18284b2586ed4d91390cf26abc3255d4
Frederico Jeronimo <frederico.jeronimo@red-gate.com>
2019-11-06 11:19:58 +0000

    Update to SSMS 18.4+

--- manifests/ssms/v2018.pp
+++ manifests/ssms/v2018.pp
@@ -1,8 +1,8 @@
-# Install SSMS 18.3
+# Install latest SSMS (18.4 as of 06/11/2019)
 class sqlserver::ssms::v2018(
-  $source = 'https://go.microsoft.com/fwlink/?linkid=2105412',
+  $source = 'https://aka.ms/ssmsfullsetup',
   $filename = 'SSMS-Setup-ENU.exe',
-  $programName = 'Microsoft SQL Server Management Studio - 18.3',
+  $programName = 'Microsoft SQL Server Management Studio - 18.4+',
   $tempFolder = 'c:/temp'
   ) {
 
