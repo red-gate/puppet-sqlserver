2a361116ab6ec7b42fbf788659e9a4a87eb40f3b
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2019-11-20 10:42:28 +0000

    Rename parameter to just password

--- manifests/users/login_sql.pp
+++ manifests/users/login_sql.pp
@@ -1,7 +1,7 @@
 # Create a SQL login
-define sqlserver::users::login_sql($server, $login_name, $login_password, $query_username = undef, $query_password = undef) {
+define sqlserver::users::login_sql($server, $login_name, $password, $query_username = undef, $query_password = undef) {
 
-  $sql_query = "CREATE LOGIN [${login_name}] WITH PASSWORD = '${login_password}'"
+  $sql_query = "CREATE LOGIN [${login_name}] WITH PASSWORD = '${password}'"
 
   ::sqlserver::sqlcmd::sqlquery { "${server} - Create ${login_name} login - SQL Server Auth":
     server   => $server,
