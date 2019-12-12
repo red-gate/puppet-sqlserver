20010ac16fc505bad9da020abf4b2124be09664e
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2019-12-12 10:04:35 +0000

    Add resources to setup SQL Server 2008 R2

---
+++ manifests/v2008r2/instance.pp
@@ -0,0 +1,47 @@
+# Install an configure a single SQL Server 2008 R2 Instance.
+#
+# $install_type: 'RTM', 'SP3'
+#
+define sqlserver::v2008r2::instance(
+  $instance_name  = $title,
+  $install_type   = 'SP3',
+  $install_params = {},
+  $tcp_port       = 0
+  ) {
+
+  require ::sqlserver::v2008r2::iso
+
+  Exec {
+    path    => 'C:/Windows/System32',
+    timeout => 1800,
+  }
+
+  $default_parameters = {
+    sqlsvcaccount => 'NT AUTHORITY\NetworkService',
+    agtsvcaccount => 'NT AUTHORITY\NetworkService',
+  }
+
+  sqlserver::common::install_sqlserver_instance { $instance_name:
+    installer_path => $::sqlserver::v2008r2::iso::installer,
+    install_params => deep_merge($default_parameters, $install_params),
+    quiet_params   => '/QUIET', # SQL Server 2008 r2 does not know about /IACCEPTSQLSERVERLICENSETERMS
+  }
+
+  # 'Patch' is equivalent to 'SP4' for backwards compatibility
+  if $install_type == 'Patch' or $install_type == 'SP3' {
+    require ::sqlserver::v2008r2::sp3
+
+    sqlserver::common::patch_sqlserver_instance { $instance_name:
+      instance_name      => $instance_name,
+      installer_path     => $::sqlserver::v2008r2::sp3::installer,
+      applies_to_version => $::sqlserver::v2008r2::sp3::applies_to_version,
+    }
+  }
+
+  if $tcp_port > 0 {
+    sqlserver::common::tcp_port { $instance_name:
+      tcp_port => $tcp_port,
+    }
+  }
+
+}
