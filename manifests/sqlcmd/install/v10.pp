9e2f2607aadce0f21a4628e91a86c97a833d50f6
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2020-03-20 10:28:01 +0000

    Update broken download urls.

--- manifests/sqlcmd/install/v10.pp
+++ manifests/sqlcmd/install/v10.pp
@@ -4,11 +4,11 @@ class sqlserver::sqlcmd::install::v10($temp_folder = 'C:/Windows/Temp') {
   include archive
 
   if $::architecture == 'x86' {
-    $nativeclient_source = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x86/sqlncli.msi'
-    $sqlcmdutils_source = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x86/SqlCmdLnUtils.msi'
+    $nativeclient_source = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x86/sqlncli.msi'
+    $sqlcmdutils_source = 'https://download.microsoft.com/download/9/2/7/927B0C39-C3E2-4CFC-B84E-92BC63344C62/ENU/x86/SqlCmdLnUtils.msi'
   } else {
-    $nativeclient_source = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/sqlncli.msi'
-    $sqlcmdutils_source = 'http://download.microsoft.com/download/B/6/3/B63CAC7F-44BB-41FA-92A3-CBF71360F022/1033/x64/SqlCmdLnUtils.msi'
+    $nativeclient_source = 'https://download.microsoft.com/download/2/4/F/24FE862D-7D32-47F2-B91D-22DAFA270BBC/2008%20R2%20ENU-1033/x64/sqlncli.msi'
+    $sqlcmdutils_source = 'https://download.microsoft.com/download/9/2/7/927B0C39-C3E2-4CFC-B84E-92BC63344C62/ENU/x64/SqlCmdLnUtils.msi'
   }
 
   archive { "${temp_folder}/sqlncli_v10.msi":
