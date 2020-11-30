4e27060de1884e7f8f67cfa07b3db30d1bd722c9
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2020-11-30 11:08:39 +0000

    Fix version number of SQL 2008 SP4.

--- manifests/v2008/jan2018cu.pp
+++ manifests/v2008/jan2018cu.pp
@@ -2,7 +2,7 @@
 # SQL Server 2008 build versions: https://sqlserverbuilds.blogspot.co.uk/
 class sqlserver::v2008::jan2018cu(
   $source = 'https://download.microsoft.com/download/7/B/5/7B5E5ED1-0976-497D-B691-ED0D7311C4B8/SQLServer2008-KB4057114-x64.exe',
-  $applies_to_version = '10.0.6000.29'
+  $applies_to_version = '10.4.6000.29'
   ) {
   # $installer points to setup.exe
   $installer = inline_template('<%= "C:/Windows/Temp/" + File.basename(@source, ".*") + "/setup.exe" %>')
--- manifests/v2008/sp4.pp
+++ manifests/v2008/sp4.pp
@@ -1,4 +1,4 @@
-# Download and extract SQL Server 2008 Sp4. (10.0.6000.29)
+# Download and extract SQL Server 2008 Sp4. (10.4.6000.29)
 # SQL Server 2008 build versions: https://sqlserverbuilds.blogspot.co.uk/
 class sqlserver::v2008::sp4(
   $source = 'https://download.microsoft.com/download/5/E/7/5E7A89F7-C013-4090-901E-1A0F86B6A94C/ENU/SQLServer2008SP4-KB2979596-x64-ENU.exe',
