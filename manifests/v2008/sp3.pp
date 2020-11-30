215ccbe0fd46c2d665af87bbca00ab99dd03b270
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2020-11-30 11:19:22 +0000

    Revert "Fix build version."
    This reverts commit a2a87ee455ffebf7759a603ea8465389f028504a.
    
    Pretty sure that the SP3 installer we download will set the PatchLevel to 10.3.5500.0.

--- manifests/v2008/sp3.pp
+++ manifests/v2008/sp3.pp
@@ -1,4 +1,4 @@
-# Download and extract SQL Server 2008 Sp3. (10.0.5500.0)
+# Download and extract SQL Server 2008 Sp3. (10.3.5500.0)
 # SQL Server 2008 build versions: https://sqlserverbuilds.blogspot.co.uk/
 class sqlserver::v2008::sp3(
   $source = 'https://download.microsoft.com/download/7/C/8/7C8F6A23-5876-4C55-91AD-5488360F8FD6/SQLServer2008SP3-KB2546951-x64-ENU.exe',
