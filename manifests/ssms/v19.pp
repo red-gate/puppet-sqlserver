3777cf51e11ae62508c4833b5a9a08c97f3b6689
Owen Standage <owen.standage@red-gate.com>
2022-07-25 09:41:40 +0100

    Add SSMS 19 definition

---
+++ manifests/ssms/v19.pp
@@ -0,0 +1,22 @@
+# Install SSMS 19 Preview 2
+class sqlserver::ssms::v19(
+  $source = 'https://go.microsoft.com/fwlink/?linkid=2195969&clcid=0x409',
+  $filename = 'SSMS-Setup-ENU.exe',
+  $programName = 'Microsoft SQL Server Management Studio 19',
+  $tempFolder = 'C:/Windows/Temp',
+  ) {
+
+  include archive
+
+  archive { "${tempFolder}/${filename}":
+    source  => $source,
+  }
+  -> reboot { 'reboot before installing SSMS (if pending)':
+    when => pending,
+  }
+  -> package { $programName:
+    ensure          => installed,
+    source          => "${tempFolder}/${filename}",
+    install_options => ['/install', '/quiet', '/norestart'],
+  }
+}
