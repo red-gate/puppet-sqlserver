5042901b8dc2b115e5d78bf0cb13b8fab34559e7
Nick Howell <nick.howell@red-gate.com>
2023-10-16 13:34:56 +0100

    fix SSMS tests

--- spec/vagrantfile.rb
+++ spec/vagrantfile.rb
@@ -1,4 +1,6 @@
 Vagrant.configure(2) do |config|
+  config.winrm.transport = "plaintext"
+  config.winrm.basic_auth_only = true
   config.vm.provision 'shell', inline: <<-SHELL
     "Disabling windows update service..."
     sc.exe config wuauserv start= disabled
