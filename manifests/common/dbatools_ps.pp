90ea5acf4b99382b7b2f9ef5fac6bf7ffa8b7ba7
Nick Howell <nick.howell@red-gate.com>
2023-10-20 17:08:45 +0100

    don't need this after all

--- manifests/common/dbatools_ps.pp
+++
@@ -1,9 +0,0 @@
-# @summary Installs the DBA Tools powershell module
-#
-class sqlserver::common::dbatools_ps () {
-  require chocolatey
-  package { 'dbatools':
-    ensure => latest,
-    provider => chocolatey,
-  }
-}
