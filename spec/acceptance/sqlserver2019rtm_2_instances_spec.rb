b8611397acbd2477c887de2b59e5d21e8f611074
Nick Howell <nick.howell@red-gate.com>
2023-10-23 15:08:30 +0100

    update 2019 for new cert method

--- spec/acceptance/sqlserver2019rtm_2_instances_spec.rb
+++ spec/acceptance/sqlserver2019rtm_2_instances_spec.rb
@@ -18,10 +18,10 @@ end
     it { should exist }
     it { should have_property_value('PatchLevel', :type_string, '15.0.2000.5') }
   end
+end
 
-  describe windows_registry_key("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SQL Server\\MSSQL15.#{instance_name}\\Mssqlserver\\Supersocketnetlib") do
-    it { should have_property_value('Certificate', :type_string, '3401AEE89B13985BFE3BEFFDE853D574E0243E09') }
-  end
+describe windows_registry_key("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SQL Server\\MSSQL15.#{instance_name}\\Mssqlserver\\Supersocketnetlib") do
+  it { should have_property_value('Certificate', :type_string, '3401AEE89B13985BFE3BEFFDE853D574E0243E09') }
 end
 
 describe windows_registry_key('HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL15.SQL2019_1\Setup') do
