53ec546a2c386a05870a629b113fde31d8f5e9c1
Nick Howell <nick.howell@red-gate.com>
2023-10-23 15:10:15 +0100

    update 2022 for new cert method

--- manifests/v2022/instance.pp
+++ manifests/v2022/instance.pp
@@ -13,11 +13,14 @@
 # @param tcp_port
 #   Specify the TCP port to listen on 
 #
+# @param certificate_thumbprint
+#   Thumbprint of an SSL cert in the local certificate store to use for SQL Connections
 define sqlserver::v2022::instance (
   String $instance_name = $title,
   String $install_type = 'Patch',
   Hash $install_params = {},
   Integer $tcp_port = 0,
+  Optional[String] $certificate_thumbprint = undef,
 ) {
   require sqlserver::v2022::iso
 
@@ -29,6 +32,7 @@ define sqlserver::v2022::instance (
   sqlserver::common::install_sqlserver_instance { $instance_name:
     installer_path => $sqlserver::v2022::iso::installer,
     install_params => $install_params,
+    certificate_thumbprint => $certificate_thumbprint,
   }
 
 # Patch is not yet supported for SQL Server 2022, so do just act like base install
