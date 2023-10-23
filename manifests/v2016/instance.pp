98956f5fe376fc17a33771f0b39b0b745a526928
Nick Howell <nick.howell@red-gate.com>
2023-10-23 15:03:01 +0100

    lint 2016

--- manifests/v2016/instance.pp
+++ manifests/v2016/instance.pp
@@ -1,15 +1,25 @@
-# Install an configure a single SQL Server 2016 Instance.
+# @summary Install an configure a single SQL Server 2016 Instance.
 #
-# $install_type: 'RTM' or 'SP1'
+# @param instance_name 
+#   Name of the instance being installed
 #
-define sqlserver::v2016::instance(
-  $instance_name  = $title,
-  $install_type   = 'SP1',
-  $install_params = {},
-  $tcp_port       = 0
-  ) {
-
-  require ::sqlserver::v2016::iso
+# @param install_type
+#   Type of install. Specify a Patch level to also install the related patch.
+#   Can be RTM, SP3, SP4 or Jan2018CU
+#
+# @param install_params
+#   Hash of install parameters to pass to the SQL installer
+#
+# @param tcp_port
+#   Specify the TCP port to listen on 
+#
+define sqlserver::v2016::instance (
+  String $instance_name = $title,
+  String $install_type = 'SP1',
+  Hash $install_params = {},
+  Integer $tcp_port       = 0
+) {
+  require sqlserver::v2016::iso
 
   Exec {
     path    => 'C:/Windows/System32',
@@ -17,17 +27,17 @@ define sqlserver::v2016::instance(
   }
 
   sqlserver::common::install_sqlserver_instance { $instance_name:
-    installer_path => $::sqlserver::v2016::iso::installer,
+    installer_path => $sqlserver::v2016::iso::installer,
     install_params => $install_params,
   }
 
   # 'Patch' is equivalent to 'SP1' for backwards compatibility
   if $install_type == 'Patch' or $install_type == 'SP1' {
-    require ::sqlserver::v2016::sp1
+    require sqlserver::v2016::sp1
 
     sqlserver::common::patch_sqlserver_instance { $instance_name:
-      installer_path     => $::sqlserver::v2016::sp1::installer,
-      applies_to_version => $::sqlserver::v2016::sp1::applies_to_version,
+      installer_path     => $sqlserver::v2016::sp1::installer,
+      applies_to_version => $sqlserver::v2016::sp1::applies_to_version,
     }
   }
 
@@ -36,5 +46,4 @@ define sqlserver::v2016::instance(
       tcp_port => $tcp_port,
     }
   }
-
 }
