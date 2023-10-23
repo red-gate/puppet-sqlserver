75a3cb73f2e96e6405a5595a6f9961cc11c602d0
Nick Howell <nick.howell@red-gate.com>
2023-10-23 15:04:24 +0100

    update 2016 for setting ssl cert

--- manifests/v2016/instance.pp
+++ manifests/v2016/instance.pp
@@ -13,11 +13,14 @@
 # @param tcp_port
 #   Specify the TCP port to listen on 
 #
+# @param certificate_thumbprint
+#   Thumbprint of an SSL cert in the local certificate store to use for SQL Connections
 define sqlserver::v2016::instance (
   String $instance_name = $title,
   String $install_type = 'SP1',
   Hash $install_params = {},
-  Integer $tcp_port       = 0
+  Integer $tcp_port = 0,
+  Optional[String] $certificate_thumbprint = undef,
 ) {
   require sqlserver::v2016::iso
 
@@ -29,6 +32,7 @@ define sqlserver::v2016::instance (
   sqlserver::common::install_sqlserver_instance { $instance_name:
     installer_path => $sqlserver::v2016::iso::installer,
     install_params => $install_params,
+    certificate_thumbprint => $certificate_thumbprint,
   }
 
   # 'Patch' is equivalent to 'SP1' for backwards compatibility
