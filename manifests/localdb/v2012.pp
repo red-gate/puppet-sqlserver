51652b2eabb9ca8e803eb27b6f736c7a77ca150e
Nick Howell <nick.howell@red-gate.com>
2023-10-24 13:11:50 +0100

    force `version` value to be a string

--- manifests/localdb/v2012.pp
+++ manifests/localdb/v2012.pp
@@ -7,7 +7,7 @@ class sqlserver::localdb::v2012 (
 ) {
   sqlserver::localdb::install { 'Install LocalDB 2012':
     source     => 'https://download.microsoft.com/download/8/D/D/8DD7BDBA-CEF7-4D8E-8C16-D9F69527F909/ENU/x64/SqlLocalDB.MSI',
-    version    => 2012,
+    version    => '2012',
     temp_folder => $temp_folder,
   }
   # make sure that the v11.0 auto instance is created and is valid.
--- manifests/localdb/v2014.pp
+++ manifests/localdb/v2014.pp
@@ -7,7 +7,7 @@ class sqlserver::localdb::v2014 (
 ) {
   sqlserver::localdb::install { 'Install LocalDB 2014':
     source     => 'https://download.microsoft.com/download/E/A/E/EAE6F7FC-767A-4038-A954-49B8B05D04EB/LocalDB%2064BIT/SqlLocalDB.msi',
-    version    => 2014,
+    version    => '2014',
     temp_folder => $temp_folder,
   }
   # make sure that the MSSQLLocalDB auto instance is created and is valid.
