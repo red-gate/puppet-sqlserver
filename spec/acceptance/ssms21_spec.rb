69c94880b4796fddbc73e847161991a442ef56c2
Nick Howell <nick.howell@red-gate.com>
2025-05-22 11:31:47 +0100

    fix package name in test

--- spec/acceptance/ssms21_spec.rb
+++ spec/acceptance/ssms21_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_windowshelper'
 
-describe package('Microsoft SQL Server Management Studio - 21.*') do
+describe package('SQL Server Management Studio - 21*') do
   it { should be_installed}
 end
